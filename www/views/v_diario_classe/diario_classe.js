1cfa87413d9752ab691e4c915c4be3a1cb738b7c
Rian Perassoli <rian.perassoli@gmail.com>
2015-06-17 23:23:00 -0300

    Presenças funcionando corretamente

--- www/views/v_diario_classe/diario_classe.js
+++ www/views/v_diario_classe/diario_classe.js
@@ -149,53 +149,81 @@ angular.module('ClazzManager.diario_classe', ['ngRoute'])
 }])
 .controller('PresencaCtrl', ['$scope', '$routeParams', '$location', function($scope, $routeParams, $location){
     $scope.alunos = [];
-    var presenca = {codigo:null, aula:'', pessoa:0, dataHora:'', situacao:'', observacao:''};
+//    var presenca = {codigo:null, aula:'', pessoa:0, dataHora:'', situacao:'', observacao:''};
     $scope.db = prepareDatabase();
     
     function listarAlunos(){
-        $scope.listaVazia = '';
         $scope.db.transaction(function(t) {
             t.executeSql("SELECT * FROM Pessoa where pessoa_tipo = ? and situacao = ?", [1, 'ativo'], function(t, results) {
                 $scope.alunos.splice(0, $scope.alunos.length);
+                
                 for (var i = 0; i < results.rows.length; i++) {
-                  var record = results.rows.item(i);                  
-                  $scope.alunos.push({ aluno:record, presente:false, obs:'' });                   
-                }
-                $scope.$apply();
+                    var record = results.rows.item(i);                  
+                    $scope.alunos.push({ aluno:record, presente:false, obs:'', presencaCodigo:null });
+                    
+                    t.executeSql("SELECT * FROM presenca where aula = ? and pessoa = ?", [$routeParams.codigo, $scope.alunos[i].aluno.codigo], 
+                    function(t, presenca){
+                        if (presenca.rows.length > 0){
+                            for (var j = 0; j< $scope.alunos.length; j++){
+                                if ($scope.alunos[j].aluno.codigo === presenca.rows.item(0).pessoa){
+                                    $scope.alunos[j].presencaCodigo = presenca.rows.item(0).codigo;
+                                    $scope.alunos[j].presente = (presenca.rows.item(0).situacao === 'presente');
+                                    $scope.alunos[j].obs = presenca.rows.item(0).observacao;
+                                    break;
+                                };
+                            };                        
+                        };
+                        
+                        $scope.$apply();
+                    }, function(t, e) {               
+                        alert('Erro: ' + e.message);
+                    });
+                };
+                
+                $scope.$apply();             
             }, function(t, e) {               
-                $scope.listaVazia = 'Não possuem alunos cadastradas';
-                $scope.$apply();
+                alert('Erro: ' + e.message);
             });
         });
-    };
+    };  
     
     $scope.init = function(){
         listarAlunos();        
     };
     $scope.init();
    
-    $scope.salvar = function(){
+    $scope.salvar = function(){       
         $scope.db.transaction(function(t) {
+            var aula = $routeParams.codigo;
             for (var i = 0; i < $scope.alunos.length; i++){        
                 var presenca = {codigo:null, aula:'', pessoa:0, dataHora:'', situacao:'', observacao:''};
                 
-                presenca.aula = $routeParams.codigo;
+                presenca.aula = aula;
                 presenca.pessoa = $scope.alunos[i].aluno.codigo;
                 if ($scope.alunos[i].presente) 
                     presenca.situacao = 'presente';
                 else
                     presenca.situacao = 'ausente';
-                presenca.observacao = $scope.alunos[i].obs;
-
-                console.log(presenca);
-
-                t.executeSql("insert into Presenca (aula, pessoa, dataHora, situacao, observacao) values  (?, ?, DateTime(), ?, ?)", 
-                [presenca.aula, presenca.pessoa, presenca.situacao, presenca.observacao], 
-                console.log('registro salvo com sucesso. Codigo: ' + presenca.pessoa), 
-                function(t, e) {                        
-                    console.log("Error: " + e.message);
-                });                
-            }; 
+                presenca.observacao = $scope.alunos[i].obs;           
+                
+                if($scope.alunos[i].presencaCodigo === null){
+                    t.executeSql("insert into Presenca (aula, pessoa, dataHora, situacao, observacao) values  (?, ?, DateTime(), ?, ?)", 
+                    [presenca.aula, presenca.pessoa, presenca.situacao, presenca.observacao], 
+                    console.log('registro salvo com sucesso. Codigo: ' + presenca.pessoa), 
+                    function(t, e) {                        
+                        console.log("Error: " + e.message);
+                    });    
+                } else {
+                    t.executeSql("update Presenca set dataHora=DateTime(), situacao = ?, observacao=? where aula = ? and pessoa = ?",
+                    [presenca.situacao, presenca.observacao, presenca.aula, presenca.pessoa], 
+                    console.log('registro alterado com sucesso. Codigo: ' + presenca.pessoa), 
+                    function(t, e) {                        
+                        console.log("Error: " + e.message);
+                    });
+                };
+            };       
+            $location.path("/diario_classe/listar_presencas"); 
+            $scope.$apply();
         });
     };
 
